2464e129b25f73ff4014d638d21bea81df840bc5
Joseph Clyde Mar <clydemar234@gmail.com>
2023-10-08 22:22:21 +0800

    Generate QR code from payment link

--- src/pages/General/PaymentLink/PaymentLink.jsx
+++ src/pages/General/PaymentLink/PaymentLink.jsx
@@ -1,15 +1,23 @@
 // src/components/PaymentQRLink.jsx
 import './PaymentLink.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const PaymentLink = () => {
-    const paymentURL = "https://schoolpaymentlink.com"; // Replace With School's payment link
-
+    const [paymentLink, setPaymentLink] = useState(''); // Replace With School's payment link
+    useEffect(() => {
+        fetch('/api/school').then(result => {
+            return result.json();
+        }).then(value => {
+            console.log(value.PaymentLink);
+            setPaymentLink(value.paymentLink);
+        });
+        
+    }, []);
     return (
         <div className= "PaymentLink">
             <h1>Welcome to ABC School!</h1>
             <h2>Scan to Pay</h2>
-            <img src="/img/qrcode.png" alt="Description of Image"/>
+            <img src={ `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${paymentLink}` } alt="Description of Image"/>
         </div>
     );
 }

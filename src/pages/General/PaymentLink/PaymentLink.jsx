b65110e9334638208881a0809dae7e8289b3251d
Joseph Clyde Mar <clydemar234@gmail.com>
2023-10-13 19:08:59 +0800

    Remove the background image Calendar.png

--- src/pages/General/PaymentLink/PaymentLink.jsx
+++ src/pages/General/PaymentLink/PaymentLink.jsx
@@ -22,7 +22,7 @@ const PaymentLink = () => {
         <>
             <NavBar  schoolInfo={schoolInfo}/>
             <div className= "PaymentLink">
-                <h1>Welcome to { schoolInfo !== null ? schoolInfo.schoolName : '' }</h1>
+                <h1>Welcome to <span>{ schoolInfo !== null ? schoolInfo.schoolName : '' }</span></h1>
                 <h2>Scan to Pay</h2>
                 <img src={ `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${paymentLink}` } alt="Description of Image"/>
             </div>
